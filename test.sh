b11b5bb40e8396641e034701b821b6dc149a9827
SlightlyCodic <chiragj831@gmail.com>
2025-04-14 14:49:42 +0100

    first commit

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@
 python test.py \
   --model_name "efficientnet_b0" \
   --model_path "Models/EfficientNetB0.pth" \
-  --img_dir "data/diabetic_retinopathy" \
+  --img_dir "/kaggle/input/diabetic-retinopathy-224x224-gaussian-filtered/gaussian_filtered_images/gaussian_filtered_images/" \
   --batch_size 64 \
   --num_workers 4 \
   --loss "CrossEntropyLoss"
--- train.sh
+++ train.sh
@@ -3,7 +3,7 @@
 # Make sure to give this script execution permissions using: chmod +x train.sh
 
 python main.py \
-  --img_dir "/root/.cache/kagglehub/datasets/sovitrath/diabetic-retinopathy-224x224-gaussian-filtered/versions/2/gaussian_filtered_images/gaussian_filtered_images" \
+  --img_dir "/kaggle/input/diabetic-retinopathy-224x224-gaussian-filtered/gaussian_filtered_images/gaussian_filtered_images/" \
   --epochs 30 \
   --batch_size 64 \
   --lr 0.00001 \
